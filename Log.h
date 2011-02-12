2d0c1d730379099be45f8a26913a28c620b21810
Felix Wiemuth <felixwiemuth@hotmail.de>
2011-02-12 17:04:17 +0100

    added files from "SudokuSolver" https://github.com/felixwiemuth/SudokuSolver/commit/770cb28f2294476fe9be0feec9e3b1e5f825a387

---
+++ Log.h
@@ -0,0 +1,53 @@
+#ifndef LOG_H_INCLUDED
+#define LOG_H_INCLUDED
+
+#include <vector>
+#include <string>
+
+//struct Entry
+//{
+//    enum Entry_type
+//    {
+//        N,
+//        ERR
+//    };
+//    std::string text;
+//    Entry_type type;
+//
+//    Entry(Entry_type t, std::string tx)
+//    {
+//        text = tx;
+//        type = t;
+//    }
+//};
+
+//class to log all program acitivity
+
+class Log
+{
+    //data
+    private:
+        std::vector<std::string> logstr;
+
+    //methods declaration
+    public:
+        //constructors
+        Log();
+
+        //methods
+        bool load_log(const char path[]);
+        bool save_log(const char path[]);
+        void add(std::string s); //add normal entry
+        void err(std::string s); //add error entry
+
+        //print methods
+        void print(unsigned int entry); //displays 'logstr[entry]'
+        void print(unsigned int start, unsigned int end); //displays 'logstr[start]' to 'logstr[end]' -- if end=0 logstr[start] to logstr[entries] will be displayes
+        void print(); //displays whole content of 'logstr'
+
+    private:
+        void send_console(); //sends last entry to standard output -- used to print new log entry
+};
+
+
+#endif // LOG_H_INCLUDED
