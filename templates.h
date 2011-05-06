f7ca5ff45e7750367a8f7ef23f330bbbc693ac1f
Felix Wiemuth <felixwiemuth@hotmail.de>
2011-05-06 12:33:14 +0200

    adapted operator "<<" to work more neatly but still does not work in every cases like stringstreams...

--- templates.h
+++ templates.h
@@ -4,10 +4,10 @@
 #include "Log.h"
 
 template<class T>
-Log& operator<<(Log& log, T& data)
+std::ostream& operator<<(Log& log, T& data) //TODO does not work in all cases like stringstreams... e.g. "log << /*double val*/ * 100 << "text";" would not work
 {
     log.ref_buff() << data;
-    return log;
+    return log.ref_buff();
 }
 
 #endif // TEMPLATES_H_INCLUDED
